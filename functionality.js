1446233593c06a729a0eda744f2c9acac7eafd08
shadreza <shadreza100@gmail.com>
2021-01-30 19:38:29 +0600

    working on the individual inputs

--- functionality.js
+++ functionality.js
@@ -1,8 +1,17 @@
+var doubleFare=0;
 var firstClassFarePerSeat = parseFloat(document.getElementById('firstClassFarePerSeat').innerText);
 var economyClassFarePerSeat = parseFloat(document.getElementById('economyClassFarePerSeat').innerText);
 function updatingSubTotalAndVatAndTotal(){
     var firstClassBill = parseFloat(document.getElementById('firstClassBill').innerText);
     var economyClassBill = parseFloat(document.getElementById('economyClassBill').innerText);
+    console.log(doubleFare);
+    if(doubleFare==1){
+        document.getElementById('doubleFareNotification').style.display='block';
+        firstClassBill*=2;
+        economyClassBill*=2;
+        document.getElementById('firstClassBill').innerText=firstClassBill;
+        document.getElementById('economyClassBill').innerText=economyClassBill;
+    }
     var subTotal = firstClassBill + economyClassBill;
     var vat = (10/100) * subTotal;
     var total = subTotal + vat;
@@ -91,11 +100,44 @@ document.getElementById('plusButtonEconomyClass').addEventListener('click',funct
 document.getElementById('minusButtonEconomyClass').addEventListener('click',function(){
     changingByButton('-','numberOfEconomyClassSeats','economyClassBill',economyClassFarePerSeat)
 })
+
 document.getElementById('bookNowButton').addEventListener('click',function(){
     var fromTextValue = document.getElementById('fromText').value;
     var toTextValue = document.getElementById('toText').value;
     var departureDateValue = document.getElementById('departureDate').value;
     var returnDateValue = document.getElementById('returnDate').value;
     var totalBill = document.getElementById('totalPrice').innerText;
-    
+    var todayDate = new Date().toISOString().slice(0, 10);
+    if(fromTextValue==""){
+        alert('Please Input Flying From Location');
+        return;
+    }
+    else if(toTextValue==""){
+        alert('Please Input Destination Location');
+        return;
+    }
+    else if(fromTextValue==toTextValue){
+        alert("Departure and Destination Location can't be same");
+        return;
+    }
+    else if(departureDateValue==""){
+        alert('Please Select Departure Date');
+        return;
+    }
+    else if(todayDate>departureDateValue){
+        alert("Departure Date can't be From The Past");
+        return;
+    }
+    else if(returnDateValue!=""){
+        if(departureDateValue>returnDateValue){
+            alert("Return Date Can't Be Before The Departure Date");
+        }
+        else{
+            doubleFare=1;
+            document.getElementById('doubleFareNotification').style.display='block';
+        }
+    }
+    else{
+        doubleFare=0;
+    }
 })
