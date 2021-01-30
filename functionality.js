cccab4dd3407d057f67e87b6078eccdb157dd6f0
shadreza <shadreza100@gmail.com>
2021-01-30 21:36:19 +0600

    about everything done except the pop up window custom one

--- functionality.js
+++ functionality.js
@@ -1,17 +1,14 @@
 var doubleFare=0;
+if(doubleFare==0){
+    document.getElementById('doubleFareNotification').style.display='none';
+}
 var firstClassFarePerSeat = parseFloat(document.getElementById('firstClassFarePerSeat').innerText);
 var economyClassFarePerSeat = parseFloat(document.getElementById('economyClassFarePerSeat').innerText);
 function updatingSubTotalAndVatAndTotal(){
     var firstClassBill = parseFloat(document.getElementById('firstClassBill').innerText);
     var economyClassBill = parseFloat(document.getElementById('economyClassBill').innerText);
-    console.log(doubleFare);
-    if(doubleFare==1){
-        document.getElementById('doubleFareNotification').style.display='block';
-        firstClassBill*=2;
-        economyClassBill*=2;
-        document.getElementById('firstClassBill').innerText=firstClassBill;
-        document.getElementById('economyClassBill').innerText=economyClassBill;
-    }
+    document.getElementById('firstClassBill').innerText=firstClassBill;
+    document.getElementById('economyClassBill').innerText=economyClassBill;
     var subTotal = firstClassBill + economyClassBill;
     var vat = (10/100) * subTotal;
     var total = subTotal + vat;
@@ -22,6 +19,10 @@ function updatingSubTotalAndVatAndTotal(){
 function changingSeatCountsAndFare(numberOfSeatsOfTheClass,billOfTheClass,billPerSeatOfClass){
     var numberOfClassSeatsNode = document.getElementById(numberOfSeatsOfTheClass);
     var numberOfClassSeatsString=numberOfClassSeatsNode.value;
+    console.log(numberOfClassSeatsString);
+    if(numberOfClassSeatsString==""){
+        numberOfClassSeatsString=0;
+    }
     var numberOfClassSeats=parseInt(numberOfClassSeatsString);
     if(numberOfClassSeats<0){
         alert("Amount Of Seats Can't Be Negative!");
@@ -51,7 +52,13 @@ function changingSeatCountsAndFare(numberOfSeatsOfTheClass,billOfTheClass,billPe
         return;
     }
     numberOfClassSeatsNode.value=numberOfClassSeats;
-    document.getElementById(billOfTheClass).innerText=(billPerSeatOfClass*numberOfClassSeats);
+    if(doubleFare==1){
+        document.getElementById('doubleFareNotification').style.display='block';
+        document.getElementById(billOfTheClass).innerText=(2*billPerSeatOfClass*numberOfClassSeats);
+    }
+    else{
+        document.getElementById(billOfTheClass).innerText=(billPerSeatOfClass*numberOfClassSeats);
+    }
     updatingSubTotalAndVatAndTotal();
 }
 document.getElementById('numberOfFirstClassSeats').addEventListener('change',function(){
@@ -100,14 +107,85 @@ document.getElementById('plusButtonEconomyClass').addEventListener('click',funct
 document.getElementById('minusButtonEconomyClass').addEventListener('click',function(){
     changingByButton('-','numberOfEconomyClassSeats','economyClassBill',economyClassFarePerSeat)
 })
-
+var fromTextNode = document.getElementById('fromText');
+var toTextNode = document.getElementById('toText');
+var departureDateNode = document.getElementById('departureDate');
+var returnDateNode = document.getElementById('returnDate');
+var totalBillNode = document.getElementById('totalPrice');
+var todayDate = new Date().toISOString().slice(0, 10);
+fromTextNode.addEventListener('change',function(){
+    if(fromTextNode.value!=""){
+        if(toTextNode.value!=""){
+            if(fromTextNode.value==toTextNode.value){
+                alert('The Departure And Landing Location Can not be Same');
+            }
+            else if(fromTextNode.value==""){
+                alert('Please Select The Departure Location First');
+            }
+        }
+    }
+    else{
+        alert('Please Input Departure Location First');
+    }
+})
+toTextNode.addEventListener('change',function(){
+    if(toTextNode.value!=""){
+        if(fromTextNode.value==""){
+            alert('Please Select The Departure Location First');
+        }
+        else{
+            if(fromTextNode.value==toTextNode.value){
+                alert('The Departure And Landing Location Can not be Same');
+            }
+        }
+    }
+    else{
+        alert('Please Select Landing Location');
+    }
+})
+departureDateNode.addEventListener('change',function(){
+    if(departureDateNode.value==""){
+        alert('Please Pick Your Departure Date');
+    }
+    else{
+        if(todayDate>departureDateNode.value){
+            alert("Departure Date can't be From The Past");
+            departureDateNode.value=todayDate;
+        }
+    }
+})
+returnDateNode.addEventListener('change',function(){
+    if(returnDateNode.value==""){
+        doubleFare=0;
+        document.getElementById('doubleFareNotification').style.display='none';
+        changingSeatCountsAndFare('numberOfFirstClassSeats','firstClassBill',firstClassFarePerSeat);
+        changingSeatCountsAndFare('numberOfEconomyClassSeats','economyClassBill',economyClassFarePerSeat);
+    }
+    else{
+        if(departureDateNode.value==""){
+            alert('Please Pick Your Departure Date First');
+        }
+        else if(departureDateNode.value>returnDateNode.value){
+            alert('Return Date Must Be After The Departure Date');
+            returnDateNode.value=departureDateNode.value;
+        }
+        doubleFare=1;
+        document.getElementById('doubleFareNotification').style.display='block';
+        changingSeatCountsAndFare('numberOfFirstClassSeats','firstClassBill',firstClassFarePerSeat);
+        changingSeatCountsAndFare('numberOfEconomyClassSeats','economyClassBill',economyClassFarePerSeat);
+    }
+})
 document.getElementById('bookNowButton').addEventListener('click',function(){
-    var fromTextValue = document.getElementById('fromText').value;
-    var toTextValue = document.getElementById('toText').value;
-    var departureDateValue = document.getElementById('departureDate').value;
-    var returnDateValue = document.getElementById('returnDate').value;
-    var totalBill = document.getElementById('totalPrice').innerText;
-    var todayDate = new Date().toISOString().slice(0, 10);
+    var fromTextValue = fromTextNode.value;
+    var toTextValue = toTextNode.value;
+    var departureDateValue = departureDateNode.value;
+    var returnDateValue = returnDateNode.value;
+    var firstClassSeatsNumber = document.getElementById('numberOfFirstClassSeats').value;
+    var economyClassSeatsNumber = document.getElementById('numberOfEconomyClassSeats').value;
+    var firstClassBill = parseFloat(document.getElementById('firstClassBill').innerText);
+    var economyClassBill = parseFloat(document.getElementById('economyClassBill').innerText);
+    var totalBill = totalBillNode.innerText;
+    
     if(fromTextValue==""){
         alert('Please Input Flying From Location');
         return;
@@ -128,6 +206,10 @@ document.getElementById('bookNowButton').addEventListener('click',function(){
         alert("Departure Date can't be From The Past");
         return;
     }
+    else if(totalBill==0){
+        alert('No Tickets Are Selected');
+        return;
+    }
     else if(returnDateValue!=""){
         if(departureDateValue>returnDateValue){
             alert("Return Date Can't Be Before The Departure Date");
@@ -135,9 +217,18 @@ document.getElementById('bookNowButton').addEventListener('click',function(){
         else{
             doubleFare=1;
             document.getElementById('doubleFareNotification').style.display='block';
+            changingSeatCountsAndFare('numberOfFirstClassSeats','firstClassBill',firstClassFarePerSeat);
+            changingSeatCountsAndFare('numberOfEconomyClassSeats','economyClassBill',economyClassFarePerSeat);
+            alert('Departure Location: '+ fromTextNode.value + '\nLanding Location: ' + toTextNode.value + '\n' + 'Departure Date: ' + departureDateNode.value + '\nReturn Date: ' + returnDateNode.value + '\n\nFirst Class Ticket: ' + firstClassSeatsNumber + '\nFirst Class Fare: ' + firstClassBill + '\n\nEconomy Class Ticket: ' + economyClassSeatsNumber + '\nEconomy Class Fare: ' + economyClassBill + '\n\nTotal Bill : ' + totalBill);
+            location.reload();
         }
     }
     else{
         doubleFare=0;
+        document.getElementById('doubleFareNotification').style.display='none';
+        changingSeatCountsAndFare('numberOfFirstClassSeats','firstClassBill',firstClassFarePerSeat);
+        changingSeatCountsAndFare('numberOfEconomyClassSeats','economyClassBill',economyClassFarePerSeat);
+        alert('Departure Location: '+ fromTextNode.value + '\nLanding Location: ' + toTextNode.value + '\n' + 'Departure Date: ' + departureDateNode.value + '\nReturn Date: ' + returnDateNode.value + '\n\nFirst Class Ticket: ' + firstClassSeatsNumber + '\nFirst Class Fare: ' + firstClassBill + '\n\nEconomy Class Ticket: ' + economyClassSeatsNumber + '\nEconomy Class Fare: ' + economyClassBill + '\n\nTotal Bill : ' + totalBill);
+        location.reload();
     }
 })
